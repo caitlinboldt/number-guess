72f8d0a93291b3a006a0c489abf49bf8c7d1a2ed
caitlinboldt <caitlin@boldt.ca>
2018-05-03 09:58:22 -0700

    python number guess

---
+++ number_guess.py
@@ -0,0 +1,34 @@
+import random
+
+number = random.randint(1, 10)
+
+print ("Guess a number between 1 and 10. You will have a total of 3 guesses.")
+
+guess1 = int(raw_input("Enter guess 1: "))
+if(guess1 > number):
+  print("Too high, try again")
+elif(guess1 < number):
+  print("Too low, try again.")
+else:
+  print("You are correct! :)")
+  exit()
+  
+guess2 = int(raw_input("Enter guess 2: "))
+if(guess2 > number):
+  print("Too high, try again")
+elif(guess2 < number):
+  print("Too low, try again.")
+else:
+  print("You are correct! :)")
+  exit()
+
+guess3 = int(raw_input("Enter guess 3: "))
+if(guess3 > number):
+  print("Too high, try again")
+elif(guess3 < number):
+  print("Too low, try again.")
+else:
+  print("You are correct! :)")
+  
+
+
