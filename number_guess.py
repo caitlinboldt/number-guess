332174804f716fd56c4333d6d0f3a175d9081e10
caitlinboldt <caitlin@boldt.ca>
2018-05-03 13:36:52 -0700

    edited guess3

--- number_guess.py
+++ number_guess.py
@@ -6,7 +6,7 @@ print ("Guess a number between 1 and 10. You will have a total of 3 guesses.")
 
 guess1 = int(raw_input("Enter guess 1: "))
 if(guess1 > number):
-  print("Too high, try again")
+  print("Too high, try again.")
 elif(guess1 < number):
   print("Too low, try again.")
 else:
@@ -15,7 +15,7 @@ else:
   
 guess2 = int(raw_input("Enter guess 2: "))
 if(guess2 > number):
-  print("Too high, try again")
+  print("Too high, try again.")
 elif(guess2 < number):
   print("Too low, try again.")
 else:
@@ -24,9 +24,9 @@ else:
 
 guess3 = int(raw_input("Enter guess 3: "))
 if(guess3 > number):
-  print("Too high, try again")
+  print("Too high, game over!")
 elif(guess3 < number):
-  print("Too low, try again.")
+  print("Too low, game over!")
 else:
   print("You are correct! :)")
   
